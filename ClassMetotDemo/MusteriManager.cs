ad9421875439abd3c6ba6f5ca99fd0397256fbe1
Emre Özyüzücüler <emre.ozyuzuculer@outlook.com>
2021-01-19 03:44:35 +0300

    Y.E.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Name + musteri.Surname + "isimli müşterimize ait iban numarası:" + musteri.IBAN + "'dır.");            
+        }
+        public void Cikar(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name + musteri.Surname + "isimli müşterimize ait iban numarası:" + musteri.IBAN + "CIKARILMIŞTIR.");
+        }
+        public void listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + musteri.Name + musteri.Surname + musteri.IBAN);
+        }
+
+
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,46 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 003456;
+            musteri1.Name = ("Yunus Emre");
+            musteri1.Surname = ("ÖZYÜZÜCÜLER");
+            musteri1.IBAN = ("123456789");
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 003457;
+            musteri2.Name = ("Tuğçe");
+            musteri2.Surname = ("PAK");
+            musteri2.IBAN = ("123456788");
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 003458;
+            musteri3.Name = ("Enes");
+            musteri3.Surname = ("AYGÜN");
+            musteri3.IBAN = ("123456787");
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 003459;
+            musteri4.Name = ("Esra");
+            musteri4.Surname = ("UĞUR");
+            musteri4.IBAN = ("123456786");
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+
+            Console.WriteLine("PANEL");
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Name);
+                Console.WriteLine(musteri.Surname);
+                Console.WriteLine(musteri.IBAN);
+                Console.WriteLine("--------------------------------------");
+            }
+
         }
+
     }
 }
